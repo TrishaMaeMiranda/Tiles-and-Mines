7bb3d02264327e9a60bbe5024855f8839a91baab
Princess-Anne Pabico <118491145+CessAnne05@users.noreply.github.com>
2022-12-07 10:22:21 +0800

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,1 +1,63 @@
+from tkinter import Button, Label
+import random
+import settings
+import ctypes
+import sys
 
+class Cell:
+    all = []
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
+    def __init__(self,x, y, is_mine=False):
+        self.is_mine = is_mine
+        self.is_opened = False
+        self.is_mine_candidate = False
+        self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        # Append the object to the Cell.all list
+        Cell.all.append(self)
+
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            width=12,
+            height=4,
+        )
+        btn.bind('<Button-1>', self.left_click_actions ) # Left Click
+        btn.bind('<Button-3>', self.right_click_actions ) # Right Click
+        self.cell_btn_object = btn
+
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg='black',
+            fg='white',
+            text=f"Cells Left:{Cell.cell_count}",
+            font=("", 30)
+        )
+        Cell.cell_count_label_object = lbl
+
+    def left_click_actions(self, event):
+        if self.is_mine:
+            self.show_mine()
+        else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            self.show_cell()
+            # If Mines count is equal to the cells left count, player won
+            if Cell.cell_count == settings.MINES_COUNT:
+                ctypes.windll.user32.MessageBoxW(0, 'Congratulations! You won the game!', 'Game Over', 0)
+
+        # Cancel Left and Right click events if cell is already opened:
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
+
+    def get_cell_by_axis(self, x,y):
+        # Return a cell object based on the value of x,y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
